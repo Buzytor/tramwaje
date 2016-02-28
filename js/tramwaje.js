e03125b3b0197461e27c237c404f9aec230c3ff9
Jan Michalski <jan.michalski@buzytor.com>
2016-02-28 13:24:25 +0100

    Add line showing controls

--- js/tramwaje.js
+++ js/tramwaje.js
@@ -17,22 +17,30 @@ var speed2color = function(speed) {
 var map = null;
 jQuery(function($) {
     map = L.map('map').setView([52.232222, 21.008333], 14);
+
     var layer = L.tileLayer('http://tile.stamen.com/toner/{z}/{x}/{y}.png', {
-                    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+                    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors | Map: <a href="http://maps.stamen.com">Stamen</a>',
                     maxZoom: 25,
                     minZoom: 0,
                     crs: L.CRS.EPSG3857
                 }).addTo(map);
 
+    var layers = {};
+
     $.ajax("tram_speed.json").success(function(data) {
         data = JSON.parse(data);
         for(var line in data) {
             line_points = data[line];
+            var layerGroup = L.featureGroup();
             line_points.forEach(function(row) {
-                L.polyline([row["point1"], row["point2"]],
-                    {color: speed2color(row["speed"]), opacity: 0.9}).addTo(map);
+                layerGroup.addLayer(L.polyline([row["point1"], row["point2"]],
+                    {color: speed2color(row["speed"]), opacity: 0.9}));
             });
+            layerGroup.bindPopup(line).addTo(map);
+            layers[line] = layerGroup;
         }
+        L.control.layers(null, layers).addTo(map);
     });
+
 });
 
